1105674ef855e329c4f887d63d38317bda062110
xirlorm <xirlohm@yahoo.com>
2022-04-22 07:33:07 +0000

    first commit

---
+++ script.js
@@ -0,0 +1,128 @@
+//Books collection
+let library = [new Book("Rogue", "author", 48, true, 0)]
+
+//Book template
+function Book(title, author, pages, read, bookNumber){
+    this.title = title
+    this.author = author
+    this.pages = pages
+    this.read = read
+    this.bookNumber = bookNumber
+
+    /*this.displayInfo = function() {
+    return `${this.title} by ${this.author}\nwritten in ${this.pages}\n${this.read}`
+    */
+}
+
+
+//Add a book to the library
+function addBook() {
+    //get info to create new book
+    let title =  document.querySelector('#title').value.toUpperCase()
+    let author = document.querySelector('#author').value.toUpperCase()
+    let pages = document.querySelector('#pages').value.toUpperCase()
+    let read = document.querySelector('#read').checked
+    if((title.length == 0) || (author.length == 0) || (`${pages}`.length == 0)) {
+        displayError()
+    } else {
+        pages = `${pages} pages`
+        //create new book with current info and add it to library
+        const book = new Book(title, author, pages, read, library.length)
+        library.push(book)
+        resetForm();
+        //Hide the form visibility
+        document.querySelector('.formContainer').style.display = 'none'
+        //show user the current books stored
+        displayBooks()
+
+        const errorMsg = document.querySelector('#errorMsg')
+        errorMsg.textContent = ''
+    }
+}
+document.querySelector('#addBook').addEventListener('click', addBook)
+
+
+//Reset form
+function resetForm () {
+    //Reset the form values to nothing
+    title.value = ''
+    author.value = ''
+    pages.value = ''
+    read.checked = false
+}
+
+
+//display Error
+function displayError () {
+    const errorMsg = document.querySelector('#errorMsg')
+    errorMsg.textContent = 'Error reading empty or invalid input field!'
+    errorMsg.style.color = 'red'
+    errorMsg.style.fontFamily = 'sans-serif'
+    errorMsg.style.fontSize = '16px'
+}
+
+
+//Assign new numbers to books when one is removed
+function resetBookNumbers() {
+    let length = 0;
+    library.forEach(book => {
+        book.bookNumber = length
+        ++length
+    })
+}
+
+//Display the Books in library
+function displayBooks() {
+    let booksDisplay = document.querySelector('#booksDisplay')
+    booksDisplay.innerText = ''
+    library.forEach((book) => {
+        //Create Card
+        let bookCard = document.createElement('div')
+        bookCard.setAttribute('data-key', book.bookNumber)
+        for(let key in book) {
+            if(key == 'bookNumber') continue
+            let bookInfo = document.createElement('div')
+            bookInfo.innerText = book[key]
+            bookInfo.setAttribute('class', key)
+            if(key == 'read'){
+                let hasRead = document.createElement('input')
+                hasRead.setAttribute('type', 'checkbox')
+                hasRead.checked = book.read
+                hasRead.addEventListener('change', ()=> {
+                    library[book.bookNumber].read = !(library[book.bookNumber].read)
+                })
+                bookInfo.innerText = key[0].toUpperCase() + key.slice(1)
+                bookInfo.appendChild(hasRead)
+            }
+            bookCard.appendChild(bookInfo)
+        }
+
+        //Create delete button and add it to card
+        let deleteButton = document.createElement('button')
+        deleteButton.setAttribute('class', 'removeBook')
+        deleteButton.innerText = 'Remove Book'
+        deleteButton.addEventListener('click', () => {
+            library.splice(book.bookNumber, 1)
+            resetBookNumbers()
+            displayBooks()
+        })
+        bookCard.appendChild(deleteButton)
+        //Add bookCard to books
+        booksDisplay.appendChild(bookCard)
+    })
+}
+
+
+//Toggle Form visibility when add icon is clicked
+document.querySelector('header > span').addEventListener('click', () => {
+    const showForm = document.querySelector('.formContainer')
+    if(showForm.style.display != 'inline-block') {
+        showForm.style.display = 'inline-block'
+        return;
+    }
+    showForm.style.display = 'none'
+})
+
+// let Maquire = new Book('Maquire', 'Fred', 2002, 'read')
+// console.log(Maquire.displayInfo())
+
